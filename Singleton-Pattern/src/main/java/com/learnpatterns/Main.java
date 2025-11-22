ad8464dde47ef0e821f681616892c9a8de9d78b2
serg10arg <cabrerasergio094@gmail.com>
2025-11-22 16:57:24 -0300

    feat(singleton): implementa la estructura basica del patron singleton
    Se crea la clase RegistroUnico con un contructor privado para impedir su
    instanciacion directa.
    
    Este cambio demuestra el principio fundamental del patron Singleton,
    donde el intento de crear un objeto con new fuera de la clase
    (Main.java) resultara en un error de compilacion, asegurando que la
    instanciacion no sea posible.

---
+++ Singleton-Pattern/src/main/java/com/learnpatterns/Main.java
@@ -0,0 +1,7 @@
+package com.learnpatterns;
+
+public class Main {
+    public static void main(String[] args) {
+        RegistroUnico miObjeto = new RegistroUnico();
+    }
+}
---
+++ Singleton-Pattern/src/main/java/com/learnpatterns/RegistroUnico.java
@@ -0,0 +1,7 @@
+package com.learnpatterns;
+
+public class RegistroUnico {
+    private RegistroUnico() {
+        // Constructor privado para prevenir la instanciación.
+    }
+}
